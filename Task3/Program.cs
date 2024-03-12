0496d0e8d6e108a7ee7db6fd82427c1b17875a89
Shamil <abdulkhalickov.shamil@gmail.com>
2024-03-12 11:30:18 +0300

    Добавили решение третьего задания

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+int [] CreateRandomArray(int size, int min = 0, int max = 20) // Создание массива
+{
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)  // Вывод массива
+{
+    foreach(int item in array)
+    {
+        Console.Write($"{item}\t");
+    }
+}
+
+void ArrayReversal(int [] array, int size) // Вывод массива, начиная с конца
+{
+    if (size == 0)
+    {
+        return;
+    }
+    Console.Write($"{array[size - 1]}\t");
+    ArrayReversal(array, size - 1);
+
+}
+
+int size = 10;
+int [] array = CreateRandomArray(size);
+ShowArray(array);
+Console.WriteLine();
+ArrayReversal(array, size);
